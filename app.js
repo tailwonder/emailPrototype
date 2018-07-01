99242e675bb1f6d8fcb1314b81620c2c9eb7675e
monicadelaine <anderson@cs.ua.edu>
2018-07-01 18:32:36 -0500

    Using default sessions for login/logout management
    Need to update sessions to use redis to persist email connection
    persistence and caching of emails

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var flash = require('flash-express')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,19 +16,86 @@ var composeRouter = require('./routes/compose');
 var sentRouter = require('./routes/sent');
 var app = express();
 
+function checkAuth (req, res, next) {
+	console.log('checkAuth ' + req.url);
+  console.log('authed ' + req.session.authenticated);
+
+	// don't serve /secure to those not logged in
+	// you should add to this list, for each and every secure url
+	if (req.url === '/home' && (!req.session || !req.session.authenticated)) {
+		res.render('unauthorised', { status: 403 });
+		return;
+	}
+
+  if (req.url === '/users' && (!req.session || !req.session.authenticated)) {
+		res.render('unauthorised', { status: 403 });
+		return;
+	}
+
+  if (req.url === '/inbox' && (!req.session || !req.session.authenticated)) {
+    res.render('unauthorised', { status: 403 });
+    return;
+  }
+
+  if (req.url === '/home' && (!req.session || !req.session.authenticated)) {
+    res.render('unauthorised', { status: 403 });
+    return;
+  }
+
+  if (req.url === '/compose' && (!req.session || !req.session.authenticated)) {
+    res.render('unauthorised', { status: 403 });
+    return;
+  }
+
+  if (req.url === '/sent' && (!req.session || !req.session.authenticated)) {
+    res.render('unauthorised', { status: 403 });
+    return;
+  }
+
+  if (req.url === '/logout' || req.url === '/logout' ) {
+    delete req.session.authenticated;
+
+  }
+
+	console.log('next ');
+	next();
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+
+var sess = {
+  secret: 'keyboard cat',
+  cookie: {},
+  resave: true,
+  saveUninitialized: true
+}
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+
+app.use(session(sess))
+app.use(checkAuth);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/logoff', loginRouter);
+app.use('/logout', loginRouter);
 app.use('/home', homeRouter);
 app.use('/inbox', inboxRouter);
 app.use('/compose', composeRouter);
--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,26 @@ var router = express.Router();
 // ------
 router.get('/', function(req, res, next) {
   res.render('Loginpage', {title :"Senior Citizen Mail login", message: "Please Login",text: "Welcome to Senior Citizen Mail Login", text2: "Username" });
-}); 
+});
 
+
+router.post('/', function (req, res, next) {
+
+  console.log('headers:' + JSON.stringify(req.headers))
+  console.log('in login:' + JSON.stringify(req.body))
+
+  // you might like to do a database look-up or something more scalable here
+  if (req.body.username && req.body.username === 'user' && req.body.password && req.body.password === 'pass') {
+    console.log('auth ok')
+    req.session.authenticated = true;
+    req.session.save()
+    res.redirect('/home');
+  } else {
+    res.flash('error', 'Username and password are incorrect');
+    //req.session.authenticated = false;
+    //req.session.save()
+    res.redirect('/login');
+  }
+});
 
 module.exports = router;
